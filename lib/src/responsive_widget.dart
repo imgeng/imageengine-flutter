a2d4684e28cdf4288a2bf478cfcf0984db4c29d3
Aditya Kantipudi <akantipudi@loginsoft.com>
2024-10-22 12:17:47 +0530

    fix: remove uneccesary headers

--- lib/src/responsive_widget.dart
+++ lib/src/responsive_widget.dart
@@ -49,10 +49,6 @@ class ResponsiveImage extends StatelessWidget {
         
         return Image.network(
           imageUrl,
-          headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-            // Add other headers if necessary
-          },
         );
       },
     );
